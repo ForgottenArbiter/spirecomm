c5ccc1683c9bb990f454fd18ee471d85fb476371
ForgottenArbiter <43520548+ForgottenArbiter@users.noreply.github.com>
2019-05-15 19:32:04 -0500

    Add setup.py (v0.4.0)

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='spirecomm',
+    version='0.4.0',
+    packages=['spirecomm', 'spirecomm.ai', 'spirecomm.spire', 'spirecomm.communication'],
+    url='https://github.com/ForgottenArbiter/spirecomm',
+    license='MIT License',
+    author='ForgottenArbiter',
+    author_email='forgottenarbiter@gmail.com',
+    description='A package for interfacing with Slay the Spire through Communication Mod',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
